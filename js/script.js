9546be8632c27c728138623f1567280dc458d506
homenada <30457819+homenada@users.noreply.github.com>
2017-09-09 11:59:39 +0300

    Добавлен JS для всплывающего окна

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var link = document.querySelector(".feedback-button");
+
+var popup = document.querySelector(".modal-feedback");
+
+var close = popup.querySelector(".modal-close");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
