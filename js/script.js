d0117c40e7647ea9157a576a0200f594cf040f5e
homenada <30457819+homenada@users.noreply.github.com>
2017-09-14 20:58:47 +0300

    Доработка JS и карты

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ var popup = document.querySelector(".modal-feedback");
 
 var close = popup.querySelector(".modal-close");
 
+var form = popup.querySelector("form");
+var username = popup.querySelector("[name=username]");
+var email = popup.querySelector("[name=email]");
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
@@ -12,4 +16,19 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+  
+form.addEventListener("submit", function (evt) {
+  console.log("OK");
+  console.log("usr: "+username.value);
+  console.log("email: "+email.value);
+  console.log("logic: "+(!username.value || !email.value));
+  if (!username.value || !email.value) {
+    evt.preventDefault();
+	console.log("Нужно ввести логин и пароль");
+    popup.classList.add("modal-error");
+  } else {
+    popup.classList.remove("modal-error");
+  }
 });
